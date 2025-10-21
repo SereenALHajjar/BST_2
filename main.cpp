263a0d20a6b5200a98acb2e43b367c393b53375a
sereen <sereen.alhajjar@gmail.com>
2025-10-21 19:48:13 +0200

    editing the root

--- main.cpp
+++ main.cpp
@@ -3,9 +3,8 @@
 #include <vector>
 // using namespace std ;
 
-using byte_array_t = std::vector<uint8_t>;
-using key_t = byte_array_t;
-using value_t = byte_array_t;
+using key_t = std::vector<uint8_t>;
+using value_t = std::vector<uint8_t>;
 
 using crefkey_t = const key_t &;
 using crefvalue_t = const value_t &;
@@ -20,27 +19,30 @@ struct Node
 };
 class BST
 {
-    public:
-    Node root;
+public:
+    Node *root;
 
     BST()
     {
-        root.right = root.left = nullptr;
+        root = nullptr;
     }
-    void print_inorder(Node *cur_node)
+    void print_inorder()
+    {
+        print_inorder_imp(root);
+    }
+    void print_inorder_imp(Node *cur_node)
     {
         if (cur_node == nullptr)
             return;
         for (auto x : cur_node->key)
-            std::cout << (char)x;
+            std::cout << int(x);
         std::cout << " ";
         for (auto x : cur_node->value)
-            std::cout << x;
+            std::cout << int(x);
         std::cout << std::endl;
-        print_inorder(cur_node->left);
+        print_inorder_imp(cur_node->left);
         std::cout << std::endl;
-        print_inorder(cur_node->right);
-        std::cout << std::endl;
+        print_inorder_imp(cur_node->right);
     }
 
     int comparision(crefkey_t a, crefkey_t b)
@@ -53,72 +55,86 @@ class BST
             else if (b[i] > a[i])
                 return -1;
         }
-        if(a.size() > b.size()) return 1 ; 
-        if(b.size() > a.size()) return -1 ; 
-        return 0 ;
+        if (a.size() > b.size())
+            return 1;
+        if (b.size() > a.size())
+            return -1;
+        return 0;
     }
     void put(crefkey_t key, crefvalue_t value)
     {
-        put_impl(&root, key, value);
+        root = put_impl(root, key, value);
     }
-    void put_impl(Node *cur_node, crefkey_t key, crefvalue_t value)
+
+    Node *put_impl(Node *cur_node, crefkey_t key, crefvalue_t value)
     {
         if (cur_node == nullptr)
         {
-            Node new_node;
-            new_node.key = key;
-            new_node.value = value;
-            new_node.left = new_node.right = nullptr;
-            cur_node = &new_node;
+            Node *new_node = new Node;
+            new_node->key = key;
+            new_node->value = value;
+            new_node->left = new_node->right = nullptr;
+            return new_node;
+        }
+
+        if (comparision(cur_node->key, key) == 1)
+        {
+            cur_node->left = put_impl(cur_node->left, key, value);
+        }
+        else if (comparision(cur_node->key, key) == -1)
+        {
+            cur_node->right = put_impl(cur_node->right, key, value);
+        }
+        else
+        {
+            cur_node->value = value;
+        }
+        return cur_node;
+    }
+    value_t get_impl(Node *cur_node, crefkey_t key)
+    {
+        if (cur_node == nullptr)
+        {
+            value_t not_found;
+            return not_found;
         }
         else if (comparision(cur_node->key, key) == 1)
-            put_impl(cur_node->left, key, value);
+            return get_impl(cur_node->left, key);
         else if (comparision(cur_node->key, key) == -1)
-            put_impl(cur_node->right, key, value);
+            return get_impl(cur_node->right, key);
         else
-            cur_node->value = value;
-    }
-
-    value_t get_impl(Node *cur_node , crefkey_t key)
-    {
-        if(cur_node == nullptr)
-        {
-            value_t not_found ; 
-            return not_found ; 
-        }
-        else if(comparision(cur_node->key , key) == 1)
-            return get_impl(cur_node->left , key) ;
-        else if(comparision(cur_node->key , key) == -1)
-            return get_impl(cur_node->right , key) ;
-        else return cur_node->value ; 
+            return cur_node->value;
     }
     value_t get(crefkey_t key)
     {
-        return get_impl(&root , key) ;
+        return get_impl(root, key);
     }
-
 };
-
-byte_array_t from_str(std::string s) {
-    return byte_array_t(s.begin(), s.end());
-}
-
 int main()
 {
 
     BST tree;
 
-    tree.put(from_str("one"), from_str("one"));
-    
-    tree.put(from_str("two"), from_str("two"));
+    key_t key1 = {1, 2, 3};
+    value_t value1 = {10, 20};
 
-    tree.put(from_str("three"), from_str("three"));
+    key_t key2 = {4, 5, 6};
+    value_t value2 = {30, 40};
+
+    key_t key3 = {2, 3, 4};
+    value_t value3 = {50, 60};
+
+    tree.put(key1, value1);
+    tree.put(key2, value2);
+    tree.put(key3, value3);
 
     std::cout << "Inorder traversal:\n";
-    tree.print_inorder(&tree.root);
+    tree.print_inorder();
 
-    value_t result = tree.get(from_str("two"));
+    key_t get_key = {4, 5, 6};
+    value_t result = tree.get(get_key);
 
+    std::cout << "\nGet value for key {4,5,6}: ";
     for (auto b : result)
         std::cout << (int)b << " ";
     std::cout << std::endl;
